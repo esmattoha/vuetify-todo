db569f940cbab0a82efa513a3967ca9a59a31131
Esmattoha Mondal <72984742+esmattoha@users.noreply.github.com>
2021-01-05 21:25:31 -0800

    Work with task todo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>To help in this task you will be guided by a translator who is an expert in the 
+      language that is the focus for that session, and a professional poet. They will offer
+       insight into the nuances of the language and culture, and give helpful suggestions for
+        the direction of the translation that is produced.
+
+      Taking place online via Google Meet, the workshops are the perfect way to keep yourself feeling
+      creative, engaged and connected to the world at large. A rough and ready guide translation is provided
+       by the guest translator so there is no need to know the language being translated – simply sign up and 
+       bring your love of poetry and language.</p>
+       <p>To help in this task you will be guided by a translator who is an expert in the 
+      language that is the focus for that session, and a professional poet. They will offer
+       insight into the nuances of the language and culture, and give helpful suggestions for
+        the direction of the translation that is produced.
+
+      Taking place online via Google Meet, the workshops are the perfect way to keep yourself feeling
+      creative, engaged and connected to the world at large. A rough and ready guide translation is provided
+       by the guest translator so there is no need to know the language being translated – simply sign up and 
+       bring your love of poetry and language.</p>
   </div>
 </template>
